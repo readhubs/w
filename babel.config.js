a5c773fe1cf704553270d991f43bff2d49370349
readhubs <drmicky299+5@gmail.com>
2026-06-03 08:30:03 +0300

    00

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,7 @@ module.exports = function (api) {
   return {
     presets: [["babel-preset-expo", { unstable_transformImportMeta: true }]],
     plugins: [
-      "react-native-reanimated/plugin",
+      "react-native-worklets/plugin",
     ],
   };
 };
